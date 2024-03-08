c26f4df24e0c0be572a5838c4443b13bb64444e2
Diegobrix <dishi.ata@gmail.com>
2024-03-08 19:30:37 -0300

    fix(project): corrigida a estrutura do projeto

--- app/pages/admin/misc/addRecipe.php
+++ app/pages/admin/misc/addRecipe.php
@@ -21,21 +21,31 @@
       </header>
       <main>
          <form action="" method="POST" enctype="multipart/form-data">
-            <section data-step="0"></section>
-            <section data-step="1"></section>
-            <section data-step="2"></section>
-            <section data-step="3"></section>
+            <section class="form_step" data-step="0" data-current="true">
+               <label for="txtTitle">Título</label>
+               <input required type="text" name="recipe_title" id="txtTitle">
+               <label for="txtDescription">Descrição</label>
+               <textarea required name="recipe_description" id="txtDescription"></textarea>
+            </section>
+            <section class="form_step" data-step="1" data-current="false">
+
+               <input required type="text" name="test">
+            </section>
+            <section class="form_step" data-step="2" data-current="false"></section>
+            <section class="form_step" data-step="3" data-current="false"></section>
+            
+            <div class="form_footer">
+               <button type="button" class="step-handler btn_prev" data-action="prev">< Anterior</button>
+               <button type="button" class="step-handler btn_next" data-action="next">Próximo ></button>
+               <button type="submit" class="step-handler btn_finish" data-action="finish">Finalizar</button>
+            </div>
          </form>
-      </main>
-      <footer>
          <div class="steps-wrapper">
             <div class="step" data-step="0" data-current="true" data-already="false"></div>
             <div class="step" data-step="1" data-current="false" data-already="false"></div>
             <div class="step" data-step="2" data-current="false" data-already="false"></div>
             <div class="step" data-step="3" data-current="false" data-already="false"></div>
          </div>
-         <button class="step-handler btn_prev" data-action="prev">< Anterior</button>
-         <button class="step-handler btn_next" data-action="next">Próximo ></button>
-      </footer>
+      </main>
    </body>
 </html>
