fc3c9bb415287343dc5e58eb803a60c88ebca815
Diegobrix <dishi.ata@gmail.com>
2024-03-20 16:38:10 -0300

    fix(php): corrigido a forma como o ingrediente é cadastrado

--- app/bd-conn-controller/pages/misc/addIngredientDB.php
+++ app/bd-conn-controller/pages/misc/addIngredientDB.php
@@ -11,8 +11,52 @@
       {
          $jsonData = file_get_contents("php://input");
          $data = json_decode($jsonData, true);
-
          
+         //region functions
+         function verifyIngredient($conn, $ingredient)
+         {
+            $stmt = $conn -> prepare("SELECT * FROM `ingrediente` WHERE descricaoIngrediente = :ingredient;");
+            $stmt -> bindParam(":ingredient", $ingredient);
+            $stmt -> execute();
+
+            if($stmt -> rowCount() > 0)
+            {
+               return false;
+            }
+
+            return true;
+         }
+
+         function registerIngredient($conn, $ingredient)
+         {
+            $stmt = $conn -> prepare("INSERT INTO `ingrediente`(descricaoIngrediente) VALUES(:ingredient);");
+            $stmt -> bindParam(":ingredient", $ingredient);
+            $stmt -> execute();
+
+            if($stmt -> rowCount() > 0)
+            {
+               return true;
+            }
+
+            return false;
+         }
+         //endregion
+
+         if($data != null)
+         {
+            $ingredient = $data["ingredient_title"];
+            if(verifyIngredient($conn, strtolower($ingredient)))
+            {
+               if(registerIngredient($conn, $data['ingredient_title']))
+               {
+                  $response['status'] = "success";
+               }
+            }
+            else
+            {
+               $response['error'] = "Ingrediente já cadastrado!";
+            }
+         }
       }
    }
    
