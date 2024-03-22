3f16a05c38976d50d0a8d55b8d71dfb09845c09d
Diegobrix <dishi.ata@gmail.com>
2024-03-21 21:20:56 -0300

    feat(api): sistema para cadastrar uma nova categoria - finalizado

--- app/bd-conn-controller/pages/misc/addContent/addCategoryDB.php
+++ app/bd-conn-controller/pages/misc/addContent/addCategoryDB.php
@@ -10,11 +10,36 @@
       require_once("./verifyContent/verifyContentExists.php");
       require_once(__DIR__."/../../../../../conf/defineSourcePath.php");
 
+      $rawData = file_get_contents("php://input");
+      $data = json_decode($rawData, true);
 
+      if($data != null)
+      {
+         if(!verifyIfExists($conn, "categoria", $data['category_title'], "descricaoCategoria"))
+         {
+            $stmt = $conn -> prepare("INSERT INTO `categoria`(descricaoCategoria) VALUES(:content);");
+            $stmt -> bindParam(":content", $data['category_title']);
+            $stmt -> execute();
+
+            if($stmt -> rowCount() > 0)
+            {
+               //Cadastrou com sucesso
+               $response['status'] = "success";
+            }
+            else
+            {
+               //Erro ao cadastrar
+            }
+         }
+         else
+         {
+            finishHim($response, "A categoria informada já cadastrada no sistema!");
+         }
+      }
+
+      echo json_encode($response);
    }
    else
    {
       finishHim($response, "Método Incorreto");
    }
-
-   echo json_encode($response);
