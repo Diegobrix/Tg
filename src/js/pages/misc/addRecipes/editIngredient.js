df1236979680a5fc1eca3d572091ae2d82fb2319
Diegobrix <dishi.ata@gmail.com>
2024-04-05 19:47:57 -0300

    fix(js): erro que impedia o usuário de remover ingredientes da receita - corrigido

--- src/js/pages/misc/addRecipes/editIngredient.js
+++ src/js/pages/misc/addRecipes/editIngredient.js
@@ -1,27 +1,32 @@
 const EDIT_INGREDIENTS = document.querySelector(".ingredients_edit");
-const BTNS_REMOVE_INGREDIENT = document.querySelectorAll(".btn_remove"); 
 const INGREDIENTS = document.querySelectorAll(".ingredient");
 
 EDIT_INGREDIENTS.addEventListener("click", () => {
+   editListener();
+});
+
+function editListener()
+{
+   const BTNS_REMOVE_INGREDIENT = document.querySelectorAll(".btn_remove"); 
    BTNS_REMOVE_INGREDIENT.forEach(btn => {
       let curState = btn.ariaHidden;
       btn.ariaHidden = curState == "true"?"false":"true";
    });
-});
 
-BTNS_REMOVE_INGREDIENT.forEach(btn => {
-   btn.addEventListener("click", (event) => {
-      event.preventDefault();
-
-      let parent = btn.parentElement;
-      let ingredientTitle = parent.querySelector(".ingredient_title").innerText;
-
-      if(confirm(`Deseja mesmo remover o ingrediente "${ingredientTitle}" da receita?`))
-      {
-         parent.remove();
-      }
+   BTNS_REMOVE_INGREDIENT.forEach(btn => {
+      btn.addEventListener("click", (event) => {
+         event.preventDefault();
+   
+         let parent = btn.parentElement;
+         let ingredientTitle = parent.querySelector(".ingredient_title").innerText;
+   
+         if(confirm(`Deseja mesmo remover o ingrediente "${ingredientTitle}" da receita?`))
+         {
+            parent.remove();
+         }
+      });
    });
-});
+}
 
 const INGREDIENT_MODAL = document.getElementById("ingredient-modal");
 INGREDIENTS.forEach(ingredient => {
