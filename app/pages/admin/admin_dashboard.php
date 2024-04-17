5e3093f97f365875ad20584ec24e82be8e9cfffb
Diegobrix <dishi.ata@gmail.com>
2024-04-17 19:44:41 -0300

    style(admin Dashboard): adicionado responsividade à tela

--- app/pages/admin/admin_dashboard.php
+++ app/pages/admin/admin_dashboard.php
@@ -41,16 +41,16 @@
    <script defer src="../../../src/js/pages/hamburger-menu.js"></script>
 </head>
 <body>
+   <?php
+      $content = new ContentController();
+      
+      $contentType = null;
+      if((isset($choosedTypeId)) && ($choosedTypeId != null))
+      {
+         $contentType = $content->defineContentType($choosedTypeId);
+      }
+   ?>
    <header>
-      <?php
-         $content = new ContentController();
-         
-         $contentType = null;
-         if((isset($choosedTypeId)) && ($choosedTypeId != null))
-         {
-            $contentType = $content->defineContentType($choosedTypeId);
-         }
-      ?>
       <section class="mobile_header-container">
          <button id="mobile_menu--handler"></button>
          <h1 class="current_data_show-display"><?=$choosedType?></h1>
@@ -76,6 +76,32 @@
             <li class="nav_item" data-current="<?=$choosedTypeId==2?'true':'false'?>" data-item-index="3"><a href="?content-type=Categorias&content-type-id=2">Categorias</a></li>
             <li class="nav_item" data-current="<?=$choosedTypeId==3?'true':'false'?>" data-item-index="4"><a href="?content-type=Vídeos&content-type-id=3">Vídeos</a></li>
          </ul>
+         <div class="stats-container">
+            <div class="bar">
+               <div style="--stat_amount: .11;" class="stat stat_recipes"></div>
+               <div style="--stat_amount: .3;" class="stat stat_ingredients"></div>
+               <div style="--stat_amount: .2;" class="stat stat_categories"></div>
+               <div style="--stat_amount: .9;" class="stat stat_videos"></div>
+            </div>
+            <div class="stats_legend-container">
+               <div class="stat_legend-wrapper">
+                  <i class="stat-thumb stat_recipes-thumb"></i>
+                  <span>Receitas</span>
+               </div>
+               <div class="stat_legend-wrapper">
+                  <i class="stat-thumb stat_ingredients-thumb"></i>
+                  <span>Ingredientes</span>
+               </div>
+               <div class="stat_legend-wrapper">
+                  <i class="stat-thumb stat_categories-thumb"></i>
+                  <span>Categorias</span>
+               </div>
+               <div class="stat_legend-wrapper">
+                  <i class="stat-thumb stat_videos-thumb"></i>
+                  <span>Vídeos</span>
+               </div>
+            </div>
+         </div>
       </section>
    </header>
    <main>
@@ -98,6 +124,7 @@
          <?php
          if($contentType != null)
          {
+            //require_once("./data/dashboard_data/dashboard_");
             $typeLabel = strtolower(substr($choosedType, 0, -1));
             foreach($contentType as $data)
             { 
@@ -107,9 +134,10 @@
                   <?php
                      if(($choosedTypeId == 0) || ($choosedTypeId == 3))
                      {
+                        $thumb = htmlspecialchars($data['thumb']);
                   ?>
                         <figure>
-                           <img src="../../../assets/images/recipes/<?=$choosedTypeId == 3?'video/'.$data['thumb']:$data['thumb']?>" alt="">
+                           <img src="../../../assets/images/recipes/<?=$choosedTypeId == 3?'video/'.$thumb:$thumb?>" alt="">
                         </figure>
                   <?php
                      }
