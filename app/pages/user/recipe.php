8372a5df320cc3134372647655d07e2ed60a5e63
Diegobrix <dishi.ata@gmail.com>
2024-04-28 00:55:35 -0300

    style(recipe): agora o header fica 'colado' no topo da página, mesmo quando o usuário scrolla a página

--- app/pages/user/recipe.php
+++ app/pages/user/recipe.php
@@ -3,12 +3,13 @@
    <head>
       <meta charset="utf-8"/>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
       <title>Receita</title>
 
       <link rel="stylesheet" type="text/css" href="../../../src/css/reset.css"/>
       <link rel="stylesheet" type="text/css" href="../../../src/css/pages/recipe-styles.css"/>
 
-      <script src="../../../src/js/pages/hamburger-menu.js"></script>
+      <script defer src="../../../src/js/pages/hamburger-menu.js"></script>
    </head>
    <body>
       <header>
@@ -25,13 +26,11 @@
                <li class="nav_item" data-current="<?=$choosedTypeId==3?'true':'false'?>" data-item-index="4"><a href="?content-type=Vídeos&content-type-id=3">Vídeos</a></li>
             </ul>
          </nav>
-         <div class="bg"></div>
+         <div class="b"></div>
          <hr>
       </header>
       <main>
-         <img src="../../../assets/images/teste_receita.jpg" alt="">
-         <img src="../../../assets/images/teste_receita.jpg" alt="">
-         <!-- <section aria-label="Resumo da receita">
+         <section aria-label="Resumo da receita">
             <figure class="img_thumb">
                <img src="../../../assets/images/teste_receita.jpg" alt="">
             </figure>
@@ -42,78 +41,6 @@
                <span class="recipe_category">Massas</span>
             </div>
          </section>
-         <section aria-label="Ingredientes e modo de preparo">
-            <section aria-labelledby="ingredients_section-title" class="ingredients_section">
-               <h2 id="ingredients_section-title">Ingredientes</h2>
-               <div>
-                  <div class="amount_ingredients-wrapper">
-                     <span>É o suficiente?</span>
-                     <button class="amount-handler" data-action="plus"></button>
-                     <span class="amount-display">1</span>
-                     <button class="amount-handler" data-action="minus"></button>
-                  </div>
-                  <ul class="ingredients-container">
-                     <template id="ingredients-template">
-                        <li>
-                           <span class="ingredient-title"></span>
-                           <span class="ingredient-amount">
-                              <span class="amount-unit"></span>
-                           </span>
-                        </li>
-                     </template>
-                     <li>
-                        <span class="ingredient-title">Ovo</span>
-                        <span class="ingredient-amount">6<span class="amount-unit">un</span></span>
-                     </li>
-                     <li>
-                        <span class="ingredient-title">Farinha</span>
-                        <span class="ingredient-amount">100<span class="amount-unit">g</span></span>
-                     </li>
-                     <li>
-                        <span class="ingredient-title">Leite</span>
-                        <span class="ingredient-amount">50<span class="amount-unit">ml</span></span>
-                     </li>
-                     <li>
-                        <span class="ingredient-title">Fermento</span>
-                        <span class="ingredient-amount">8<span class="amount-unit">g</span></span>
-                     </li>
-                     <li>
-                        <span class="ingredient-title">Açúcar</span>
-                        <span class="ingredient-amount">520<span class="amount-unit">g</span></span>
-                     </li>
-                  </ul>      
-               </div>
-               <section aria-labelledby="waytodo_section-title">
-                  <h2 id="waytodo_section-title">Modo de Preparo</h2>
-                  <div>
-                     <ul class="waytodo-container">
-                        <template id="waytodo-template">
-                           <li>
-                              <span class="sequence-index"></span>
-                              <span class="waytodo-description"></span>
-                           </li>
-                        </template>
-                        <li>
-                           <span class="sequence-index">1</span>
-                           <span class="waytodo-description">Aqueça uma frigideira em fogo médio</span>
-                        </li>
-                        <li>
-                           <span class="sequence-index">2</span>
-                           <span class="waytodo-description">Aqueça uma frigideira em fogo médio</span>
-                        </li>
-                        <li>
-                           <span class="sequence-index">3</span>
-                           <span class="waytodo-description">Aqueça uma frigideira em fogo médio</span>
-                        </li>
-                        <li>
-                           <span class="sequence-index">4</span>
-                           <span class="waytodo-description">Aqueça uma frigideira em fogo médio</span>
-                        </li>
-                     </ul>
-                  </div>
-               </section>
-            </section>
-         </section> -->
       </main>
    </body>
 </html>
