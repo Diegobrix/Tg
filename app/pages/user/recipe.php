06decb7f2c02dc24e34b03e1f688529a2e0c5527
Diegobrix <dishi.ata@gmail.com>
2024-04-28 14:56:16 -0300

    style(recipe): layout do cabeçalho e section de ingredientes para mobile - finalizado

--- app/pages/user/recipe.php
+++ app/pages/user/recipe.php
@@ -35,11 +35,67 @@
                <img src="../../../assets/images/teste_receita.jpg" alt="">
             </figure>
             <div class="abstract_content-container">
-               <h1 class="recipe-title">Macarrrrão</h1>
+               <h1 class="recipe-title">Macarrão</h1>
                <p class="recipe-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam quia vel. Provident natus officia, odit quam facilis recusandae quas molestiae aperiam magnam numquam ullam perferendis, eum, in reprehenderit tempore.
                Reiciendis vel earum commodi quidem ipsa saepe eos maxime exercitationem repellendus veniam aspernatur dignissimos voluptas sit id libero voluptates alias dolores, inventore nihil quis autem officiis. Libero autem minima ullam?</p>
                <span class="recipe-category">Massas</span>
             </div>
+         </section>
+         <section aria-label="Ingredientes e modo de preparo">
+            <section aria-labelledby="ingredients_section-title" class="ingredients_section">
+               <h2 id="ingredients_section-title">Ingredientes</h2>
+               <div>
+                  <ul class="ingredients-container">
+                     <template id="ingredients-template">
+                        <li>
+                           <span class="ingredient-title"></span>
+                           <span class="ingredient-amount">
+                              <span class="amount-unit"></span>
+                           </span>
+                        </li>
+                     </template>
+                     <li>
+                        <span class="ingredient-title">Ovo</span>
+                        <span class="ingredient-amount">6<span class="amount-unit">un</span></span>
+                     </li>
+                     <li>
+                        <span class="ingredient-title">Farinha</span>
+                        <span class="ingredient-amount">100<span class="amount-unit">g</span></span>
+                     </li>
+                     <li>
+                        <span class="ingredient-title">Leite</span>
+                        <span class="ingredient-amount">50<span class="amount-unit">ml</span></span>
+                     </li>
+                     <li>
+                        <span class="ingredient-title">Fermento</span>
+                        <span class="ingredient-amount">8<span class="amount-unit">g</span></span>
+                     </li>
+                     <li>
+                        <span class="ingredient-title">Açúcar</span>
+                        <span class="ingredient-amount">520<span class="amount-unit">g</span></span>
+                     </li>
+                  </ul> 
+                  <div class="amount_controller-wrapper">
+                     <span>É o suficiente?</span>
+                     <button class="amount-handler" data-action="plus"></button>
+                     <span class="amount-display" data-amount="1"></span>
+                     <button class="amount-handler" data-action="minus"></button>
+                  </div>     
+               </div>
+            </section>
+            <section aria-labelledby="waytodo_section-title">
+               <h2 id="waytodo_section-title">Modo de Preparo</h2>
+               <div>
+                  <ul class="waytodo-container">
+                     <template id="waytodo-template">
+                        <li>
+                           <span class="sequence-index"></span>
+                           <span class="waytodo-description"></span>
+                        </li>
+                     </template>
+                  </ul>
+               </div>
+            </section>
          </section>
       </main>
    </body>
