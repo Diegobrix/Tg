83a03011e0823c397eb21e2e6cce0fdd8b2cce99
Diegobrix <dishi.ata@gmail.com>
2024-05-08 13:42:41 -0300

    feat(searchResult page): estrutura html - finalizada

---
+++ app/pages/user/searchResult.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+   <head>
+      <meta charset="utf-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      
+      <title>Resultado da Pesquisa</title>
+
+      <link rel="stylesheet" type="text/css" href="../../../src/css/reset.css"/>
+      <link rel="stylesheet" type="text/css" href="../../../src/css/pages/searchResult/searchResult-styles.css"/>
+   </head>
+   <body>
+      <header>
+         <div class="header_head-wrapper">
+            <button id="mobile_menu--handler"></button>
+            <a href="" class="logo"><span>DIABETES</span><br>Sob Controle</a>
+         </div>
+         <nav class="mobile-menu" aria-expanded="false">
+            <ul>
+               <li>Voltar ao Inicio</li>
+            </ul>
+         </nav>
+         <div class="bg"></div>
+         <nav class="desktop-menu" aria-expanded="false">
+            <ul>
+               <li>Voltar ao Inicio</li>
+            </ul>
+         </nav>
+         <hr>
+      </header>
+      <main>
+         <section class="search-container">
+            <div class="searchbar-wrapper">
+               <i class="searchbar-icon"></i>
+               <input type="text" name="" id="searchbar" placeholder="Macarrão">
+            </div>
+            <h1>Resultados para "Macarrão"</h1>
+         </section>
+         <section class="results-container">
+            <div class="options-container">
+               <div class="filters-container">
+                  <div class="filter_add-wrapper">
+                     <select name="" id="" class="filter-select">
+                        <option selected disabled value="#">Filtros</option>
+                     </select>
+                  </div>
+               </div>
+               <div class="results-container">
+               </div>
+               <div class="display_mode-container">
+                  <button class="display_mode list-display"></button>
+                  <button class="display_mode grid-display"></button>
+               </div>
+            </div>
+         </section>
+      </main>
+   </body>
+</html>
